16e187e3f5b094e7942b1cd437bd711e73b3acb0
Alejandro Melara <alejandro.melara96@gmail.com>
2023-03-07 15:05:01 -0500

    Fix utils test, to work properly

--- src/tests/utils.test.js
+++ src/tests/utils.test.js
@@ -2,10 +2,10 @@ import { formatMinutesAndSeconds } from "../utils";
 
 test("formatMinutesAndSeconds", () => {
   expect(formatMinutesAndSeconds(0)).toBe("0:00");
-  expect(formatMinutesAndSeconds(0)).toBe("0:01");
-  expect(formatMinutesAndSeconds(0)).toBe("1:00");
-  expect(formatMinutesAndSeconds(0)).toBe("1:01");
-  expect(formatMinutesAndSeconds(0)).toBe("59:59");
-  expect(formatMinutesAndSeconds(0)).toBe("60:00");
-  expect(formatMinutesAndSeconds(0)).toBe("60:01");
+  expect(formatMinutesAndSeconds(1)).toBe("0:01");
+  expect(formatMinutesAndSeconds(60)).toBe("1:00");
+  expect(formatMinutesAndSeconds(61)).toBe("1:01");
+  expect(formatMinutesAndSeconds(3599)).toBe("59:59");
+  expect(formatMinutesAndSeconds(3600)).toBe("60:00");
+  expect(formatMinutesAndSeconds(3601)).toBe("60:01");
 });
