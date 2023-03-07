b21843a7ffaa054976016221a17b59fb54e105d3
Alejandro Melara <alejandro.melara96@gmail.com>
2023-03-07 15:12:23 -0500

    It shows properly formatted minutes and seconds

--- src/tests/Scoreboard.test.jsx
+++ src/tests/Scoreboard.test.jsx
@@ -1,7 +1,15 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Scoreboard from "../components/Scoreboard";
 
-test("renders scoreboard", () => {
-  const rendered = render(<Scoreboard />);
+test("initial render", () => {
+  const rendered = render(<Scoreboard timeRemaining={1} />);
   expect(rendered).toMatchSnapshot();
+});
+
+it("shows properly formatted minutes and seconds", () => {
+  render(<Scoreboard timeRemaining={899} />);
+
+  const timeP = screen.getByTestId("time");
+
+  expect(timeP).toHaveTextContent("14:59");
 });
